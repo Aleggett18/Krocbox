a86e3d93a19a8134d6a11e960e62dd33f81606b6
Aleggett18 <56698715+Aleggett18@users.noreply.github.com>
2026-03-08 16:00:34 -0400

    Update wallpaper.sh

--- files/scripts/wallpaper.sh
+++ files/scripts/wallpaper.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 mkdir -p /usr/share/backgrounds/krocbox
-cp /tmp/files/wallpaper.png /usr/share/backgrounds/krocbox/krocbox-wallpaper.png
 
-mkdir -p /usr/share/glib-2.0/schemas
+cp /files/wallpaper/krocbox-wallpaper.png /usr/share/backgrounds/krocbox/krocbox-wallpaper.png
+
+
