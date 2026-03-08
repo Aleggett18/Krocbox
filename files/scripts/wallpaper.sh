722651d156003934a4ba2d7fdec9aae40cf24320
Aleggett18 <56698715+Aleggett18@users.noreply.github.com>
2026-03-08 15:52:07 -0400

    Update wallpaper.sh

--- files/scripts/wallpaper.sh
+++ files/scripts/wallpaper.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
 
 mkdir -p /usr/share/backgrounds/krocbox
-
 cp /tmp/files/wallpaper.png /usr/share/backgrounds/krocbox/krocbox-wallpaper.png
 
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/krocbox/wallpaper.png"
+mkdir -p /usr/share/glib-2.0/schemas
