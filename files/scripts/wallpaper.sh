5d60e61591e224eafbf4427577f6e6f6a0adb43a
Aleggett18 <56698715+Aleggett18@users.noreply.github.com>
2026-03-08 16:19:15 -0400

    Delete files/scripts/wallpaper.sh

--- files/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p /usr/share/backgrounds/krocbox
-
-cp /tmp/files/wallpaper/krocbox-wallpaper.png /usr/share/backgrounds/krocbox/krocbox-wallpaper.png
-
-
