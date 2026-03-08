3140aef932fc6a55c99543c34fc08234e05f1388
Aleggett18 <56698715+Aleggett18@users.noreply.github.com>
2026-03-08 12:41:33 -0400

    Update branding.sh

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -2,5 +2,6 @@ set -e
 
 echo "Applying Krocbox branding..."
 
-cp /files/branding/krocbox-logo.png /usr/share/pixmaps/krocbox-logo.png
-cp /usr/share/pixmaps/krocbox-logo.png /usr/share/pixmaps/fedora-gdm-logo.png
+install -D /files/branding/krocbox-logo.png /usr/share/pixmaps/krocbox-logo.png
+
+ln -sf /usr/share/pixmaps/krocbox-logo.png /usr/share/pixmaps/fedora-gdm-logo.png
